6a4cb1d12e9c59d67a4b658de21e26cb0b5db0e3
janko <janko.simonovic@sbgenomics.com>
2016-09-15 10:48:22 +0200

    fix upload with cache

--- rabix-executor/src/main/java/org/rabix/executor/handler/impl/JobHandlerImpl.java
+++ rabix-executor/src/main/java/org/rabix/executor/handler/impl/JobHandlerImpl.java
@@ -242,7 +242,13 @@ public class JobHandlerImpl implements JobHandler {
       Map<String, Object> results = localMemoizationService.tryToFindResults(job);
       if (results != null) {
         job = Job.cloneWithOutputs(job, results);
-        uploadOutputFiles(job, bindings);
+        
+        Set<FileValue> fileValues = bindings.getProtocolFiles(workingDir);
+        Set<File> files = new HashSet<>();
+        for (FileValue fileValue : fileValues) {
+          files.add(new File(fileValue.getPath()));
+        }
+        uploadService.upload(files, storageConfiguration.getPhysicalExecutionBaseDir(), true, true, job.getConfig());
         job = bindings.mapOutputFilePaths(job, outputFileMapper);
         return job;
       }
