f7216917a7576863137c17302177ac2994faa74b
sinisa <sinisa.ivkovic@sbgenomics.com>
2016-09-27 20:22:37 +0200

    preserve null in outputEval

--- rabix-bindings-draft3/src/main/java/org/rabix/bindings/draft3/expression/javascript/Draft3ExpressionJavascriptResolver.java
+++ rabix-bindings-draft3/src/main/java/org/rabix/bindings/draft3/expression/javascript/Draft3ExpressionJavascriptResolver.java
@@ -116,7 +116,8 @@ public class Draft3ExpressionJavascriptResolver {
       return null;
     }
     JsonNode node = JSONHelper.readJsonNode(result.toString());
-    return JSONHelper.transform(node.get("result"));
+    return JSONHelper.transformPreserveNull(node.get("result"));
+
   }
 
 }
