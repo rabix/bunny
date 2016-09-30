e916995afbc707eaee61a5322386bcd96b70e563
Janko Simonovic <janko.simonovic@sbgenomics.com>
2016-09-30 12:15:43 +0200

    handle maps for env var requirement

--- rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/bean/resource/requirement/CWLEnvVarRequirement.java
+++ rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/bean/resource/requirement/CWLEnvVarRequirement.java
@@ -3,6 +3,7 @@ package org.rabix.bindings.cwl.bean.resource.requirement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.rabix.bindings.cwl.bean.resource.CWLResource;
 import org.rabix.bindings.cwl.bean.resource.CWLResourceType;
@@ -17,18 +18,32 @@ public class CWLEnvVarRequirement extends CWLResource {
   public final static String KEY_ENV_DEF = "envDef";
 
   @JsonIgnore
+  @SuppressWarnings("unchecked")
   public List<EnvironmentDef> getEnvironmentDefinitions() {
     List<EnvironmentDef> definitions = new ArrayList<>();
 
-    List<Map<String, Object>> envDefObjs = getValue(KEY_ENV_DEF);
-    if (envDefObjs != null) {
-      for (Map<String, Object> envDefObj : envDefObjs) {
-        String name = (String) envDefObj.get(EnvironmentDef.KEY_NAME);
-        Object value = envDefObj.get(EnvironmentDef.KEY_VALUE);
-        definitions.add(new EnvironmentDef(name, value));
+    Object envDef = getValue(KEY_ENV_DEF);
+    if (envDef instanceof List<?>) {    // handle lists
+      List<Map<String, Object>> envDefObjs = (List<Map<String, Object>>) envDef;
+      if (envDefObjs != null) {
+        for (Map<String, Object> envDefObj : envDefObjs) {
+          String name = (String) envDefObj.get(EnvironmentDef.KEY_NAME);
+          Object value = envDefObj.get(EnvironmentDef.KEY_VALUE);
+          definitions.add(new EnvironmentDef(name, value));
+        }
       }
+      return definitions;
     }
-    return definitions;
+    if (envDef instanceof Map<?,?>) {   // handle maps
+      Map<String, Object> envDefObjs = (Map<String, Object>) envDef;
+      if (envDefObjs != null) {
+        for (Entry<String, Object> envDefObj : envDefObjs.entrySet()) {
+          definitions.add(new EnvironmentDef(envDefObj.getKey(), envDefObj.getValue()));
+        }
+      }
+      return definitions;
+    }
+    return null;
   }
 
   @Override
