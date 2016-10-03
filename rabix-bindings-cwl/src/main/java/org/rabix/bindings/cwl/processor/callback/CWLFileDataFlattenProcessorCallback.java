81421c9aefcea4b8beb617643bbe0459fcb61bb2
Janko Simonovic <janko.simonovic@sbgenomics.com>
2016-10-03 12:58:30 +0200

    add directory support

--- rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/processor/callback/CWLFileDataFlattenProcessorCallback.java
+++ rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/processor/callback/CWLFileDataFlattenProcessorCallback.java
@@ -1,10 +1,12 @@
 package org.rabix.bindings.cwl.processor.callback;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.rabix.bindings.cwl.helper.CWLDirectoryValueHelper;
 import org.rabix.bindings.cwl.helper.CWLFileValueHelper;
 import org.rabix.bindings.cwl.helper.CWLSchemaHelper;
 import org.rabix.bindings.cwl.processor.CWLPortProcessorCallback;
@@ -13,30 +15,48 @@ import org.rabix.bindings.model.ApplicationPort;
 
 public class CWLFileDataFlattenProcessorCallback implements CWLPortProcessorCallback {
 
-  private final Set<Map<String, Object>> flattenedFileData;
+  private final Set<Object> flattenedFileData;
 
   protected CWLFileDataFlattenProcessorCallback() {
     this.flattenedFileData = new HashSet<>();
   }
 
   @Override
-  @SuppressWarnings("unchecked")
   public CWLPortProcessorResult process(Object value, ApplicationPort port) throws Exception {
-    if (CWLSchemaHelper.isFileFromValue(value)) {
-      flattenedFileData.add((Map<String, Object>) value);
-
-      List<Map<String, Object>> secondaryFiles = CWLFileValueHelper.getSecondaryFiles(value);
-      if (secondaryFiles != null) {
-        for (Map<String, Object> secondaryFileValue : secondaryFiles) {
-          flattenedFileData.add(secondaryFileValue);
-        }
-      }
+    if (CWLSchemaHelper.isFileFromValue(value) || CWLSchemaHelper.isDirectoryFromValue(value)) {
+      flattenedFileData.addAll(flattenSingleFile(value));
       return new CWLPortProcessorResult(value, true);
     }
     return new CWLPortProcessorResult(value, false);
   }
+  
+  private Set<Object> flattenSingleFile(Object value) {
+    if (CWLSchemaHelper.isFileFromValue(value) || CWLSchemaHelper.isDirectoryFromValue(value)) {
+      Set<Object> flattenedFiles = new HashSet<>();
+      flattenedFiles.add(value);
+      
+      List<Map<String, Object>> secondaryFiles = CWLFileValueHelper.getSecondaryFiles(value);
+      if (secondaryFiles != null) {
+        for (Map<String, Object> secondaryFileValue : secondaryFiles) {
+          flattenedFiles.add(secondaryFileValue);
+        }
+      }
+      
+      if (CWLSchemaHelper.isDirectoryFromValue(value)) {
+        List<Object> listingObjs = CWLDirectoryValueHelper.getListing(value);
+        
+        if (listingObjs != null) {
+          for (Object listingObj : listingObjs) {
+            flattenedFiles.addAll(flattenSingleFile(listingObj));
+          }
+        }
+      }
+      return flattenedFiles;
+    }
+    return Collections.emptySet();
+  }
 
-  public Set<Map<String, Object>> getFlattenedFileData() {
+  public Set<Object> getFlattenedFileData() {
     return flattenedFileData;
   }
 
