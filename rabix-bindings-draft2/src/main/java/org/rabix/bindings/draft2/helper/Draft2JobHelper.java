09a93160e3f0904cd4b45f5c2f17290fa0666244
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2016-10-18 17:19:39 +0200

    remove normalize from all bindings

--- rabix-bindings-draft2/src/main/java/org/rabix/bindings/draft2/helper/Draft2JobHelper.java
+++ rabix-bindings-draft2/src/main/java/org/rabix/bindings/draft2/helper/Draft2JobHelper.java
@@ -7,7 +7,6 @@ import org.rabix.bindings.draft2.bean.Draft2JobApp;
 import org.rabix.bindings.draft2.bean.Draft2Resources;
 import org.rabix.bindings.draft2.resolver.Draft2DocumentResolver;
 import org.rabix.bindings.model.Job;
-import org.rabix.common.helper.InternalSchemaHelper;
 import org.rabix.common.helper.JSONHelper;
 import org.rabix.common.json.BeanSerializer;
 
@@ -16,7 +15,7 @@ public class Draft2JobHelper {
   public static Draft2Job getDraft2Job(Job job) throws BindingException {
     String resolvedAppStr = Draft2DocumentResolver.resolve(job.getApp());
     Draft2JobApp app = BeanSerializer.deserialize(JSONHelper.transformToJSON(resolvedAppStr), Draft2JobApp.class);
-    Draft2Job sbJob = new Draft2JobProcessor().process(new Draft2Job(InternalSchemaHelper.normalizeId(job.getName()), app, job.getInputs(), job.getOutputs()));
+    Draft2Job sbJob = new Draft2JobProcessor().process(new Draft2Job(job.getName(), app, job.getInputs(), job.getOutputs()));
 
     if (job.getResources() != null) {
       Draft2Resources sbResources = new Draft2Resources(false, job.getResources().getCpu(), job.getResources().getMemMB());
