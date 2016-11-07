0386b8391b7b79e9efdb1adcc35041e2e5afb16e
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-11-07 20:27:56 +0100

    Logger small fix

--- rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
+++ rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
@@ -23,215 +23,219 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestRunner {
-  private static String testDirPath;
-  private static String cmdPrefix;
-  private static String buildFile;
-  private static String currentTestSuite;
-  private static String resultPath = "./rabix-backend-local/target/result.yaml";
-  private static String workingdir = "./rabix-backend-local/target/";
-  private static final Logger logger = LoggerFactory.getLogger(TestRunner.class);
+	private static String testDirPath;
+	private static String cmdPrefix;
+	private static String buildFile;
+	private static String currentTestSuite;
+	private static String resultPath = "./rabix-backend-local/target/result.yaml";
+	private static String workingdir = "./rabix-backend-local/target/";
+	private static final Logger logger = LoggerFactory.getLogger(TestRunner.class);
 
-  public static void main(String[] commandLineArguments) {
-    try {
-      logger.info("Integration testing: started");	
-      PropertiesConfiguration configuration = getConfig();
-      testDirPath = getStringFromConfig(configuration, "testDirPath");
-      cmdPrefix = getStringFromConfig(configuration, "cmdPrefix");
-      buildFile = getStringFromConfig(configuration, "buildFile");
-      startTestExecution();
-      logger.info("Integration testing: finished");
-    } catch (RabixTestException e) {
-      logger.error("Error occuerred!", e);
-      System.exit(-1);
-    }
-  }
+	public static void main(String[] commandLineArguments) {
+		try {
+			logger.info("Integration testing: started");
+			PropertiesConfiguration configuration = getConfig();
+			testDirPath = getStringFromConfig(configuration, "testDirPath");
+			cmdPrefix = getStringFromConfig(configuration, "cmdPrefix");
+			buildFile = getStringFromConfig(configuration, "buildFile");
+			startTestExecution();
+			logger.info("Integration testing: finished");
+		} catch (RabixTestException e) {
+			logger.error("Error occuerred!", e);
+			System.exit(-1);
+		}
+	}
 
-  private static void startTestExecution() throws RabixTestException {
-    boolean allTestsPassed = true;
-    boolean testPassed = false;
-    File dir = new File(testDirPath);
-    File[] directoryListing = dir.listFiles();
-    ArrayList<Object> failedTests = new ArrayList<Object>();
+	private static void startTestExecution() throws RabixTestException {
+		boolean allTestsPassed = true;
+		boolean testPassed = false;
+		File dir = new File(testDirPath);
+		File[] directoryListing = dir.listFiles();
+		ArrayList<Object> failedTests = new ArrayList<Object>();
 
-    if (!dir.isDirectory()) {
-      logger.error("Problem with test directory path: Test directory path is not valid directory path.");
-      System.exit(-1);
-    }
-    if (directoryListing == null) {
-      logger.error("Problem with provided test directory: Test directory is empty.");
-    }
-    
-    logger.info("Extracting build file: started");
-    String commandUntarBuildFile = "tar -zxvf "+ System.getProperty("user.dir") + buildFile;
-    logger.info("Extracting build file command: " + commandUntarBuildFile);
-    command(commandUntarBuildFile, workingdir);
-    logger.info("Extracting build file: done");
-    
-    String commandCopyTestbacklog = "cp -a "+ System.getProperty("user.dir") + "/rabix-integration-testing/testbacklog .";
-    command(commandCopyTestbacklog, workingdir);
-    logger.info("Copying testbacklog command: " + commandCopyTestbacklog);
-    logger.info("Copying testbacklog dir: done ");
-    
-    for (File child : directoryListing) {
-      if (!child.getPath().endsWith(".test.yaml"))
-        continue;
-      try {
-    	currentTestSuite =  child.getPath();
-    	logger.info("Executing test suite: " + currentTestSuite);  
-        String currentTest = readFile(child.getAbsolutePath(), Charset.defaultCharset());
-        Map<String, Object> inputSuite = JSONHelper.readMap(JSONHelper.transformToJSON(currentTest));
-        Iterator entries = inputSuite.entrySet().iterator();
-        while (entries.hasNext()) {
-          Entry thisEntry = (Entry) entries.next();
-          Object testName = thisEntry.getKey();
-          Object test = thisEntry.getValue();
-          logger.info(" --- ");
-          logger.info("Running test: " + testName + " with given parameters:");
-          @SuppressWarnings({ "rawtypes", "unchecked" })
-          Map<String, Map<String, LinkedHashMap>> mapTest = (Map<String, Map<String, LinkedHashMap>>) test;
-          logger.info("  app: " + mapTest.get("app"));
-          logger.info("  inputs: " + mapTest.get("inputs"));
-          logger.info("  expected: " + mapTest.get("expected"));
-          String cmd = cmdPrefix + " " + mapTest.get("app") + " " + mapTest.get("inputs") + " > result.yaml";
-          logger.info("->Running cmd: " + cmd);
-          command(cmd, workingdir);
+		if (!dir.isDirectory()) {
+			logger.error("Problem with test directory path: Test directory path is not valid directory path.");
+			System.exit(-1);
+		}
+		if (directoryListing == null) {
+			logger.error("Problem with provided test directory: Test directory is empty.");
+		}
 
-          File resultFile = new File(resultPath);
+		logger.info("Extracting build file: started");
+		String commandUntarBuildFile = "tar -zxvf " + System.getProperty("user.dir") + buildFile;
+		logger.info("Extracting build file command: " + commandUntarBuildFile);
+		command(commandUntarBuildFile, workingdir);
+		logger.info("Extracting build file: done");
 
-          String resultText = readFile(resultFile.getAbsolutePath(), Charset.defaultCharset());
-          Map<String, Object> resultData = JSONHelper.readMap(JSONHelper.transformToJSON(resultText));
-          logger.info("\nGenerated result file:");
-          logger.info(resultText);
-          testPassed = validateTestCase(mapTest, resultData);
-          logger.info("Test result: ");
-          if (testPassed) {
-            logger.info(testName + " PASSED");
+		String commandCopyTestbacklog = "cp -a " + System.getProperty("user.dir")
+				+ "/rabix-integration-testing/testbacklog .";
+		command(commandCopyTestbacklog, workingdir);
+		logger.info("Copying testbacklog command: " + commandCopyTestbacklog);
+		logger.info("Copying testbacklog dir: done ");
 
-          } else {
-            logger.info(testName + " FAILED");
-            failedTests.add(testName);
-            allTestsPassed = false;
-          }
+		for (File child : directoryListing) {
+			if (!child.getPath().endsWith(".test.yaml"))
+				continue;
+			try {
 
-        }
+				currentTestSuite = child.getPath();
+				logger.info("Executing test suite: " + currentTestSuite);
+				String currentTest = readFile(child.getAbsolutePath(), Charset.defaultCharset());
+				Map<String, Object> inputSuite = JSONHelper.readMap(JSONHelper.transformToJSON(currentTest));
+				Iterator entries = inputSuite.entrySet().iterator();
 
-      } catch (IOException e) {
-        logger.error("Test suite: "+ currentTestSuite + ", execution failed. ", e);
-        System.exit(-1);
-      }
-    }
+				while (entries.hasNext()) {
+					Entry thisEntry = (Entry) entries.next();
+					Object testName = thisEntry.getKey();
+					Object test = thisEntry.getValue();
+					logger.info(" --- ");
+					logger.info("Running test: " + testName + " with given parameters:");
+					@SuppressWarnings({ "rawtypes", "unchecked" })
+					Map<String, Map<String, LinkedHashMap>> mapTest = (Map<String, Map<String, LinkedHashMap>>) test;
+					logger.info("  app: " + mapTest.get("app"));
+					logger.info("  inputs: " + mapTest.get("inputs"));
+					logger.info("  expected: " + mapTest.get("expected"));
+					String cmd = cmdPrefix + " " + mapTest.get("app") + " " + mapTest.get("inputs") + " > result.yaml";
+					logger.info("->Running cmd: " + cmd);
+					command(cmd, workingdir);
 
-    if (allTestsPassed) {
-      logger.info(" ### ");		
-      logger.info("Test suite: " + currentTestSuite + ", passed successfully.");
-    } else {
-      logger.info(" ### ");	
-      logger.info("Test suite " + currentTestSuite + ", failed:");
-      logger.info("Failed test number: " + failedTests.size());
-      logger.info("Failed tests:");
-      for (Object test : failedTests) {
-        logger.info(test.toString());
-      }
+					File resultFile = new File(resultPath);
 
-    }
-  }
+					String resultText = readFile(resultFile.getAbsolutePath(), Charset.defaultCharset());
+					Map<String, Object> resultData = JSONHelper.readMap(JSONHelper.transformToJSON(resultText));
+					logger.info("\nGenerated result file:");
+					logger.info(resultText);
+					testPassed = validateTestCase(mapTest, resultData);
+					logger.info("Test result: ");
+					if (testPassed) {
+						logger.info(testName + " PASSED");
 
-  private static boolean validateTestCase(Map<String, Map<String, LinkedHashMap>> mapTest,
-      Map<String, Object> resultData) {
-    String resultFileName;
-    int resultFileSize;
-    String resultFileClass;
-    Map<String, Object> resultValues = ((Map<String, Object>) resultData.get("outfile"));
-    resultFileName = resultValues.get("path").toString();
-    resultFileName = resultFileName.split("/")[resultFileName.split("/").length - 1];
-    resultFileSize = (int) resultValues.get("size");
-    resultFileClass = resultValues.get("class").toString();
-    logger.info("Test validation:");
-    logger.info("result file name: " + resultFileName + ", expected file name: "
-        + mapTest.get("expected").get("outfile").get("name"));
-    logger.info("result file size: " + resultFileSize + ", expected file size: "
-        + mapTest.get("expected").get("outfile").get("size"));
-    logger.info("result file class: " + resultFileClass + ", expected file class: "
-        + mapTest.get("expected").get("outfile").get("class"));
+					} else {
+						logger.info(testName + " FAILED");
+						failedTests.add(testName);
+						allTestsPassed = false;
+					}
 
-    boolean fileNamesEqual = resultFileName.equals(mapTest.get("expected").get("outfile").get("name"));
-    boolean fileSizesEqual = resultFileSize == (int) mapTest.get("expected").get("outfile").get("size");
-    boolean fileClassesEqual = resultFileClass.equals(mapTest.get("expected").get("outfile").get("class"));
+				}
 
-    if (!fileNamesEqual) {
-      logger.error("result and expected file name are not equal!");
-    } else {
-      if (!fileSizesEqual) {
-        logger.error("result and expected file size are not equal!");
-      } else {
-        if (!fileClassesEqual) {
-          logger.error("result and expected file class are not equal!");
-        } else {
-          logger.info("Test case passed.");	
-          return true;
-        }
-      }
-    }
+				if (allTestsPassed) {
+					logger.info(" ### ");
+					logger.info("Test suite: " + currentTestSuite + ", passed successfully.");
+				} else {
+					logger.info(" ### ");
+					logger.info("Test suite " + currentTestSuite + ", failed:");
+					logger.info("Failed test number: " + failedTests.size());
+					logger.info("Failed tests:");
+					for (Object test : failedTests) {
+						logger.info(test.toString());
+					}
 
-    return false;
-  }
+				}
 
-  public static void command(final String cmdline, final String directory) throws RabixTestException {
-    try {
-      Process process = new ProcessBuilder(new String[] { "bash", "-c", cmdline }).inheritIO()
-          .directory(new File(directory)).start();
+			} catch (IOException e) {
+				logger.error("Test suite: " + currentTestSuite + ", execution failed. ", e);
+				System.exit(-1);
+			}
+		}
 
-      BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      String line = null;
-      while ((line = br.readLine()) != null)
-        logger.info(line);
+	}
 
-      int exitCode = process.waitFor();
-      
-      if (0 != exitCode) {
-    	  File resultFile = new File(resultPath);
-		  String stdErr = readFile(resultFile.getAbsolutePath(), Charset.defaultCharset());
-		  logger.error(stdErr);
-    	  throw new RabixTestException("Error while executing command: Non zero exit code " + exitCode);
-      }
+	private static boolean validateTestCase(Map<String, Map<String, LinkedHashMap>> mapTest,
+			Map<String, Object> resultData) {
+		String resultFileName;
+		int resultFileSize;
+		String resultFileClass;
+		Map<String, Object> resultValues = ((Map<String, Object>) resultData.get("outfile"));
+		resultFileName = resultValues.get("path").toString();
+		resultFileName = resultFileName.split("/")[resultFileName.split("/").length - 1];
+		resultFileSize = (int) resultValues.get("size");
+		resultFileClass = resultValues.get("class").toString();
+		logger.info("Test validation:");
+		logger.info("result file name: " + resultFileName + ", expected file name: "
+				+ mapTest.get("expected").get("outfile").get("name"));
+		logger.info("result file size: " + resultFileSize + ", expected file size: "
+				+ mapTest.get("expected").get("outfile").get("size"));
+		logger.info("result file class: " + resultFileClass + ", expected file class: "
+				+ mapTest.get("expected").get("outfile").get("class"));
 
-    } catch (Exception e) {
-      logger.error("Error while executing command. ", e);
-      throw new RabixTestException("Error while executing command: " + e.getMessage());
-    }
-  }
+		boolean fileNamesEqual = resultFileName.equals(mapTest.get("expected").get("outfile").get("name"));
+		boolean fileSizesEqual = resultFileSize == (int) mapTest.get("expected").get("outfile").get("size");
+		boolean fileClassesEqual = resultFileClass.equals(mapTest.get("expected").get("outfile").get("class"));
 
-  /**
-   * Reads content from a file
-   */
-  static String readFile(String path, Charset encoding) throws IOException {
-    byte[] encoded = Files.readAllBytes(Paths.get(path));
-    return new String(encoded, encoding);
-  }
+		if (!fileNamesEqual) {
+			logger.error("result and expected file name are not equal!");
+		} else {
+			if (!fileSizesEqual) {
+				logger.error("result and expected file size are not equal!");
+			} else {
+				if (!fileClassesEqual) {
+					logger.error("result and expected file class are not equal!");
+				} else {
+					logger.info("Test case passed.");
+					return true;
+				}
+			}
+		}
 
-  @SuppressWarnings("unchecked")
-  private static PropertiesConfiguration getConfig() throws RabixTestException {
-    PropertiesConfiguration configuration = new PropertiesConfiguration();
-    String userDir = System.getProperty("user.dir");
-    if (userDir == null) {
-      throw new RabixTestException("null value for user.dir property");
-    }
-    File configDir = new File(userDir + "/rabix-integration-testing/config/test");
-    try {
-      Iterator<File> iterator = FileUtils.iterateFiles(configDir, new String[] { "properties" }, true);
-      while (iterator.hasNext()) {
-        File configFile = iterator.next();
-        configuration.load(configFile);
-      }
-      return configuration;
-    } catch (ConfigurationException e) {
-      logger.error("Failed to load configuration properties", e);
-      throw new RabixTestException("Failed to load configuration properties");
-    }
-  }
+		return false;
+	}
 
-  private static String getStringFromConfig(PropertiesConfiguration configuration, String key) {
-    return configuration.getString(key);
-  }
+	public static void command(final String cmdline, final String directory) throws RabixTestException {
+		try {
+			Process process = new ProcessBuilder(new String[] { "bash", "-c", cmdline }).inheritIO()
+					.directory(new File(directory)).start();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line = null;
+			while ((line = br.readLine()) != null)
+				logger.info(line);
+
+			int exitCode = process.waitFor();
+
+			if (0 != exitCode) {
+				File resultFile = new File(resultPath);
+				String stdErr = readFile(resultFile.getAbsolutePath(), Charset.defaultCharset());
+				logger.error(stdErr);
+				throw new RabixTestException("Error while executing command: Non zero exit code " + exitCode);
+			}
+
+		} catch (Exception e) {
+			logger.error("Error while executing command. ", e);
+			throw new RabixTestException("Error while executing command: " + e.getMessage());
+		}
+	}
+
+	/**
+	 * Reads content from a file
+	 */
+	static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static PropertiesConfiguration getConfig() throws RabixTestException {
+		PropertiesConfiguration configuration = new PropertiesConfiguration();
+		String userDir = System.getProperty("user.dir");
+		if (userDir == null) {
+			throw new RabixTestException("null value for user.dir property");
+		}
+		File configDir = new File(userDir + "/rabix-integration-testing/config/test");
+		try {
+			Iterator<File> iterator = FileUtils.iterateFiles(configDir, new String[] { "properties" }, true);
+			while (iterator.hasNext()) {
+				File configFile = iterator.next();
+				configuration.load(configFile);
+			}
+			return configuration;
+		} catch (ConfigurationException e) {
+			logger.error("Failed to load configuration properties", e);
+			throw new RabixTestException("Failed to load configuration properties");
+		}
+	}
+
+	private static String getStringFromConfig(PropertiesConfiguration configuration, String key) {
+		return configuration.getString(key);
+	}
 
 }
