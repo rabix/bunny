91e70b6a92d3185ad8ce6450e540534818790c8e
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-11-15 18:25:14 +0100

    cwlteststarter path fix

--- rabix-integration-testing/cwlteststarter.sh
+++ rabix-integration-testing/cwlteststarter.sh
@@ -4,5 +4,5 @@ source env/testenv/bin/activate
 pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
 pwd
 ls -ltra /home/travis/build/rabix/bunny/rabix-backend-local/target/
-cwltest --test conformance_test_draft-2.yaml --tool /home/travis/build/rabix/bunny/rabix-backend-local/target/rabix-backend-local-0.6.1-SNAPSHOT-id3/rabix -j 4
+cwltest --test conformance_test_draft-2.yaml --tool /home/travis/build/rabix/bunny/rabix-backend-local/target/rabix -j 4
 
