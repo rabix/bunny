da54954050f18400c9ffd39788ea72be4aaab809
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2016-11-15 14:16:08 +0100

    JobHelper fix

--- rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/helper/CWLJobHelper.java
+++ rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/helper/CWLJobHelper.java
@@ -25,10 +25,6 @@ public class CWLJobHelper {
     
     CWLJob cwlJob =  new CWLJobProcessor().process(new CWLJob(job.getName(), app, nativeInputs, nativeOutputs));
     
-    if (job.getResources() != null) {
-      CWLRuntime cwlRuntime = new CWLRuntime(job.getResources().getCpu(), job.getResources().getMemMB(), job.getResources().getWorkingDir(), job.getResources().getWorkingDir(), job.getResources().getDiskSpaceMB(), job.getResources().getDiskSpaceMB());
-      cwlJob.setRuntime(cwlRuntime);
-    }
     CWLRuntime runtime = null;
     try {
       runtime = CWLRuntimeHelper.createRuntime(cwlJob, job.getResources());
