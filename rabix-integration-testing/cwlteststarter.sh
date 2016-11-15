37786a22ad8a12d95691dc93f436438d4273e8a4
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-11-15 17:27:16 +0100

    debug cwlteststarter

--- rabix-integration-testing/cwlteststarter.sh
+++ rabix-integration-testing/cwlteststarter.sh
@@ -3,5 +3,6 @@ virtualenv env/testenv
 source env/testenv/bin/activate
 pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
 pwd
+ls -ltra /home/travis/build/rabix/bunny/rabix-backend-local/target/rabix-backend-local-0.6.1-SNAPSHOT-id3/
 cwltest --test conformance_test_draft-2.yaml --tool /home/travis/build/rabix/bunny/rabix-backend-local/target/rabix-backend-local-0.6.1-SNAPSHOT-id3/rabix -j 4
 
