1a94f31e4b021d86d86a77621852841bcca9730b
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-12-22 14:47:22 +0100

    More, 16 cwltes threads

--- rabix-integration-testing/cwlstarter/draft-2_starter.sh
+++ rabix-integration-testing/cwlstarter/draft-2_starter.sh
@@ -4,4 +4,4 @@ source env/testenv/bin/activate
 pip install -U pip setuptools wheel
 pip install pyopenssl ndg-httpsclient pyasn1
 pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
-cwltest --test conformance_test_draft-2.yaml --tool ${buildFileDirPath}/rabix -j 4
+cwltest --test conformance_test_draft-2.yaml --tool ${buildFileDirPath}/rabix -j 16
