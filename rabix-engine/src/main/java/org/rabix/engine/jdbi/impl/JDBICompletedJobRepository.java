e0563d99f2a349d169ece3daf08e1bb010fd6938
Milos Ljubinkovic <milos.ljubinkovic@sbgenomics.com>
2017-03-10 14:33:33 +0100

    dbinit script and removed status and backendid

--- rabix-engine/src/main/java/org/rabix/engine/jdbi/impl/JDBICompletedJobRepository.java
+++ rabix-engine/src/main/java/org/rabix/engine/jdbi/impl/JDBICompletedJobRepository.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 
 import org.rabix.bindings.model.Job;
 import org.rabix.bindings.model.Resources;
+import org.rabix.bindings.model.Job.JobStatus;
 import org.rabix.common.helper.JSONHelper;
 import org.rabix.engine.jdbi.impl.JDBICompletedJobRepository.CompletedJobEntityMapper;
 import org.rabix.engine.jdbi.impl.JDBIJobRepository.BindJob;
@@ -24,23 +25,21 @@ import org.skife.jdbi.v2.tweak.ResultSetMapper;
 public interface JDBICompletedJobRepository extends CompletedJobRepository {
 
   @Override
-  @SqlUpdate("insert into completed_job (id,root_id,name, parent_id, status, message, inputs, outputs, resources, backend_id, app, config) values (:id,:root_id,:name,:parent_id,:status::job_status,:message,:inputs::jsonb,:outputs::jsonb,:resources::jsonb,:backend_id,:app,:config::jsonb)")
+  @SqlUpdate("insert into completed_job (id,root_id,name, parent_id, message, inputs, outputs, resources, app, config) values (:id,:root_id,:name,:parent_id,:message,:inputs::jsonb,:outputs::jsonb,:resources::jsonb,:app,:config::jsonb)")
   void insert(@BindJob Job job, @Bind("backend_id") UUID backendId);
   
   @Override
-  @SqlUpdate("insert into completed_job (id,root_id,name, parent_id, status, message, inputs, outputs, resources, app, config) values (:id,:root_id,:name,:parent_id,:status::job_status,:message,:inputs::jsonb,:outputs::jsonb,:resources::jsonb,:app,:config::jsonb)")
+  @SqlUpdate("insert into completed_job (id,root_id,name, parent_id, message, inputs, outputs, resources, app, config) values (:id,:root_id,:name,:parent_id,:message,:inputs::jsonb,:outputs::jsonb,:resources::jsonb,:app,:config::jsonb)")
   void insert(@BindJob Job job);
 
   
-  public static class CompletedJobEntityMapper implements ResultSetMapper<CompletedJobEntity> {
-    public CompletedJobEntity map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+  public static class CompletedJobEntityMapper implements ResultSetMapper<Job> {
+    public Job map(int index, ResultSet r, StatementContext ctx) throws SQLException {
       UUID id = r.getObject("id", UUID.class);
       UUID root_id = r.getObject("root_id", UUID.class);
-      UUID backendId = r.getObject("backend_id", UUID.class);
       UUID parentId = r.getObject("parent_id", UUID.class);
       String name = r.getString("name");
       String app = r.getString("app");
-      Job.JobStatus status = Job.JobStatus.valueOf(r.getString("status"));
       String message = r.getString("message");
       String inputsJson = r.getString("inputs");
       String outputsJson = r.getString("outputs");
@@ -53,8 +52,8 @@ public interface JDBICompletedJobRepository extends CompletedJobRepository {
       Map<String, Object> outputs = JSONHelper.readMap(outputsJson);
       Map<String, Object> config = JSONHelper.readMap(configJson);
 
-      Job job = new Job(id, parentId, root_id, name, app, status, message, inputs, outputs, config, res, Collections.emptySet());
-      return new CompletedJobEntity(job, backendId);
+      Job job = new Job(id, parentId, root_id, name, app, JobStatus.COMPLETED, message, inputs, outputs, config, res, Collections.emptySet());
+      return job;
     }
   }  
 }
