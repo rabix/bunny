9228cc48736d2de079eb89a3a06258dec1e1069c
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2017-04-12 12:02:47 +0200

    don't install from master

--- rabix-integration-testing/cwlstarter/cwl_starter.sh
+++ rabix-integration-testing/cwlstarter/cwl_starter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-virtualenv -p $(which python3) env/testenv
+virtualenv -p $(which python2) env/testenv
 source env/testenv/bin/activate
 pip install -U pip setuptools wheel
 pip install pyopenssl ndg-httpsclient pyasn1
-pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
+pip install -e git+https://github.com/common-workflow-language/cwltest.git@1.0.20170214185319#egg=cwltest
 cwltest --test conformance_test_v1.0.yaml --tool ${buildFileDirPath}/rabix -j 1 --verbose
