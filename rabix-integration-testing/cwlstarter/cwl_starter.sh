ee61d7c0ffd44bb563641932c0823fccc982e55f
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2017-04-12 11:36:41 +0200

    try with python3

--- rabix-integration-testing/cwlstarter/cwl_starter.sh
+++ rabix-integration-testing/cwlstarter/cwl_starter.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-virtualenv -p $(which python2) env/testenv
+virtualenv -p $(which python) env/testenv
 source env/testenv/bin/activate
-
+pip install -U pip setuptools wheel
 pip install pyopenssl ndg-httpsclient pyasn1
-git clone https://github.com/common-workflow-language/cwltest.git
-python ./cwltest/setup.py install
+pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
 cwltest --test conformance_test_v1.0.yaml --tool ${buildFileDirPath}/rabix -j 1 --verbose
