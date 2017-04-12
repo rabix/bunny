e7b53835235a38956d9e3d2e215e7caad17c2a0e
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2017-04-12 12:32:58 +0200

    remove verbose

--- rabix-integration-testing/cwlstarter/cwl_starter.sh
+++ rabix-integration-testing/cwlstarter/cwl_starter.sh
@@ -4,5 +4,5 @@ source env/testenv/bin/activate
 pip install -U pip setuptools wheel
 pip install pyopenssl ndg-httpsclient pyasn1
 pip install typing==3.5.2.2
-pip install -e git+https://github.com/common-workflow-language/cwltest.git@1.0.20170214185319#egg=cwltest
-cwltest --test conformance_test_v1.0.yaml --tool ${buildFileDirPath}/rabix -j 1 --verbose
+pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
+cwltest --test conformance_test_v1.0.yaml --tool ${buildFileDirPath}/rabix -j 1
