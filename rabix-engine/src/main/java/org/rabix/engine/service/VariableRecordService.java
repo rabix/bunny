0bc736e5ef95f9c8040010935a44973d989d372b
Janko Simonovic <janko.simonovic@sbgenomics.com>
2017-05-30 12:26:07 +0200

    remove VariableFinder

--- rabix-engine/src/main/java/org/rabix/engine/service/VariableRecordService.java
+++ rabix-engine/src/main/java/org/rabix/engine/service/VariableRecordService.java
@@ -3,15 +3,19 @@ package org.rabix.engine.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.rabix.bindings.model.dag.DAGLinkPort;
 import org.rabix.bindings.model.dag.DAGLinkPort.LinkPortType;
 import org.rabix.storage.model.VariableRecord;
-import org.rabix.storage.model.scatter.VariableFinder;
 
-public interface VariableRecordService extends VariableFinder{
+public interface VariableRecordService {
 
   void create(VariableRecord variableRecord);
   
   void update(VariableRecord variableRecord);
+
+  VariableRecord find(String jobId, String portId, DAGLinkPort.LinkPortType type, UUID rootId);
+
+  Object getValue(VariableRecord variableRecord);
   
   List<VariableRecord> find(String jobId, LinkPortType type, UUID rootId);
   
