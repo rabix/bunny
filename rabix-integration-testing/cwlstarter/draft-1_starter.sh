36095a549020b8c39446c1025d7b4d26257ec028
Milos Ljubinkovic <milos.ljubinkovic@sbgenomics.com>
2017-07-05 12:37:05 +0200

    Attempt to fix travis, changed the install source of cwltest package

--- rabix-integration-testing/cwlstarter/draft-1_starter.sh
+++ rabix-integration-testing/cwlstarter/draft-1_starter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 virtualenv env/testenv
 source env/testenv/bin/activate
-pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
+pip install cwltest
 cwltest --test conformance_test_draft-1.yaml --tool ${buildFileDirPath}/rabix -j 1 --verbose
--- rabix-integration-testing/cwlstarter/draft-3_starter.sh
+++ rabix-integration-testing/cwlstarter/draft-3_starter.sh
@@ -3,5 +3,5 @@ virtualenv -p $(which python2) env/testenv
 source env/testenv/bin/activate
 pip install -U pip setuptools wheel
 pip install pyopenssl ndg-httpsclient pyasn1
-pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
+pip install cwltest
 cwltest --test conformance_test_draft-3.yaml --tool ${buildFileDirPath}/rabix -j 1 --verbose
