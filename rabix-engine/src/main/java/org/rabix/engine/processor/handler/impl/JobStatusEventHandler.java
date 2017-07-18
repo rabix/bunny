2bf3ea114f0a7ab0fbc1afb4792b8850d0f7d5d4
Milos Ljubinkovic <milos.ljubinkovic@sbgenomics.com>
2017-07-18 19:31:53 +0200

    Using IoC in jobhelper instead of static methods. Also reorginized to reduce duplication of code

--- rabix-engine/src/main/java/org/rabix/engine/processor/handler/impl/JobStatusEventHandler.java
+++ rabix-engine/src/main/java/org/rabix/engine/processor/handler/impl/JobStatusEventHandler.java
@@ -76,6 +76,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
   private final JobService jobService;
 
   private final boolean setResources;
+  private JobHelper jobHelper;
 
   @Inject
   public JobStatusEventHandler(final DAGNodeService dagNodeService, final AppService appService,
@@ -83,7 +84,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
       final VariableRecordService variableRecordService, final ContextRecordService contextRecordService,
       final EventProcessor eventProcessor, final ScatterHandler scatterHelper, final JobRepository jobRepository,
       final JobService jobService, final JobStatsRecordService jobStatsRecordService,
-      final Configuration configuration) {
+      final Configuration configuration, final JobHelper jobHelper) {
     this.dagNodeService = dagNodeService;
     this.scatterHelper = scatterHelper;
     this.eventProcessor = eventProcessor;
@@ -94,6 +95,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
     this.variableRecordService = variableRecordService;
     this.appService = appService;
     this.jobService = jobService;
+    this.jobHelper = jobHelper;
 
     this.jobRepository = jobRepository;
     this.setResources = configuration.getBoolean("engine.set_resources", false);
@@ -130,7 +132,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
       if (!jobRecord.isContainer() && !jobRecord.isScatterWrapper()) {
         Job job = null;
         try {
-          job = JobHelper.createReadyJob(jobRecord, JobStatus.READY, jobRecordService, variableRecordService, linkRecordService, contextRecordService, dagNodeService, appService, setResources);
+          job = jobHelper.createReadyJob(jobRecord, JobStatus.READY, setResources);
           if (!job.getName().equals(InternalSchemaHelper.ROOT_NAME)) {
             jobRepository.insert(job, event.getEventGroupId(), event.getProducedByNode());
           } else {
@@ -146,7 +148,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
       } else {
         Job containerJob = null;
         try {
-          containerJob = JobHelper.createJob(jobRecord, JobStatus.READY, jobRecordService, variableRecordService, linkRecordService, contextRecordService, dagNodeService, appService, false);
+          containerJob = jobHelper.createJob(jobRecord, JobStatus.READY, false);
         } catch (BindingException e) {
           throw new EventHandlerException("Failed to call onReady callback for Job " + containerJob, e);
         }
@@ -176,7 +178,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
             }
           }
           eventProcessor.send(new ContextStatusEvent(event.getContextId(), ContextStatus.COMPLETED));
-          Job rootJob = JobHelper.createRootJob(jobRecord, JobStatus.COMPLETED, jobRecordService, variableRecordService, linkRecordService, contextRecordService, dagNodeService, appService, event.getResult());
+          Job rootJob = jobHelper.createJob(jobRecord, JobStatus.COMPLETED, event.getResult());
           jobService.handleJobRootCompleted(rootJob);
         } catch (Exception e) {
           throw new EventHandlerException("Failed to call onRootCompleted callback for Job " + jobRecord.getRootId(), e);
@@ -210,7 +212,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
       
       if (jobRecord.isRoot()) {
         try {
-          Job rootJob = JobHelper.createRootJob(jobRecord, JobStatus.FAILED, jobRecordService, variableRecordService, linkRecordService, contextRecordService, dagNodeService, appService, null);
+          Job rootJob = jobHelper.createJob(jobRecord, JobStatus.FAILED, null);
           rootJob = Job.cloneWithMessage(rootJob, event.getMessage());
           jobService.handleJobRootFailed(rootJob);
           
@@ -220,7 +222,7 @@ public class JobStatusEventHandler implements EventHandler<JobStatusEvent> {
         }
       } else {
         try {
-          Job failedJob = JobHelper.createCompletedJob(jobRecord, JobStatus.FAILED, jobRecordService, variableRecordService, linkRecordService, contextRecordService, dagNodeService, appService);
+          Job failedJob = jobHelper.createJob(jobRecord, JobStatus.FAILED);
           failedJob = Job.cloneWithMessage(failedJob, event.getMessage());
           jobService.handleJobFailed(failedJob);
           
