9c3d606338514186805e7c286d2208da57f9f1a9
Milos Ljubinkovic <milos.ljubinkovic@sbgenomics.com>
2017-09-15 18:51:14 +0200

    Events refactor, removed persistent event type

--- rabix-engine-store-postgres/src/main/java/org/rabix/engine/store/postgres/jdbi/impl/JDBIEventRepository.java
+++ rabix-engine-store-postgres/src/main/java/org/rabix/engine/store/postgres/jdbi/impl/JDBIEventRepository.java
@@ -20,13 +20,10 @@ import org.skife.jdbi.v2.tweak.ResultSetMapper;
 public interface JDBIEventRepository extends EventRepository {
 
   @Override
-  @SqlUpdate("insert into event (id,event,type,status) select :id,:event::jsonb,:type::persistent_event_type,:status::event_status where not exists (select id from event where id=:id and type=:type::persistent_event_type)")
+  @SqlUpdate("insert into event (id,event,status) values (:id,:event::jsonb,:status::event_status)")
   void insert(@BindEvent EventRecord eventRecord);
   
-  @Override
-  @SqlUpdate("update event set status=:status::event_status,modified_at='now' where id=:id and type=:type::persistent_event_type")
-  void updateStatus(@BindEvent EventRecord eventRecord);
-  
+
   @Override
   @SqlUpdate("delete from event where id=:id")
   void deleteGroup(@Bind("id") UUID id);
@@ -37,11 +34,9 @@ public interface JDBIEventRepository extends EventRepository {
 
   public static class EventMapper implements ResultSetMapper<EventRecord> {
     public EventRecord map(int index, ResultSet r, StatementContext ctx) throws SQLException {
-      EventRecord.PersistentType type = EventRecord.PersistentType.valueOf(r.getString("type"));
       EventRecord.Status status = EventRecord.Status.valueOf(r.getString("status"));
       Map<String, ?> event = JSONHelper.readMap(r.getString("event"));
-
-      return new EventRecord(UUID.fromString((String) event.get("eventGroupId")), type, status, event);
+      return new EventRecord(UUID.fromString((String) event.get("eventGroupId")), status, event);
     }
   }
 
@@ -55,7 +50,6 @@ public interface JDBIEventRepository extends EventRepository {
           public void bind(SQLStatement<?> q, JDBIEventRepository.BindEvent bind, EventRecord event) {
             q.bind("id", event.getGroupId());
             q.bind("event", JSONHelper.writeObject(event.getEvent()));
-            q.bind("type", event.getType().toString());
             q.bind("status", event.getStatus().toString());
           }
         };
