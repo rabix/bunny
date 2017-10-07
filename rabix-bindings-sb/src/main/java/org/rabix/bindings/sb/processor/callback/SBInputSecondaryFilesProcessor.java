3e4ae30a747df23e9aa7131d460242c6bbe51bb4
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2017-10-07 14:12:58 +0200

    set secondaryFiles for inputs before staging for draft-sb

--- rabix-bindings-sb/src/main/java/org/rabix/bindings/sb/processor/callback/SBInputSecondaryFilesProcessor.java
+++ rabix-bindings-sb/src/main/java/org/rabix/bindings/sb/processor/callback/SBInputSecondaryFilesProcessor.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.rabix.bindings.model.ApplicationPort;
 import org.rabix.bindings.model.FileValue;
 import org.rabix.bindings.sb.SBProcessor;
