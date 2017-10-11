68bf14a9792e73af7e1759d44f83f5a224ab8f07
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2017-10-11 12:34:49 +0200

    dump default stderr log after postprocess

--- rabix-executor/src/main/java/org/rabix/executor/handler/impl/JobHandlerImpl.java
+++ rabix-executor/src/main/java/org/rabix/executor/handler/impl/JobHandlerImpl.java
@@ -355,19 +355,21 @@ public class JobHandlerImpl implements JobHandler {
       }
       
       String standardErrorLog = bindings.getStandardErrorLog(job);
-      if (standardErrorLog == null) {
-        standardErrorLog = DEFAULT_ERROR_FILE;
-      }
-      containerHandler.dumpContainerLogs(new File(workingDir, standardErrorLog));
-
+      
       if (!isSuccessful()) {
         uploadOutputFiles(job, bindings);
         return job;
       }
       
       filePermissionService.execute(job);
+
+      if (standardErrorLog != null) {
+        containerHandler.dumpContainerLogs(new File(workingDir, standardErrorLog));
+      }
       
       job = bindings.postprocess(job, workingDir, enableHash? hashAlgorithm : null, null);
+      
+      containerHandler.dumpContainerLogs(new File(workingDir, DEFAULT_ERROR_FILE));
       containerHandler.dumpCommandLine();
       
       statusCallback.onOutputFilesUploadStarted(job);
