58852da9d1cc76764be1e7421b5971e1b5563d6d
smiljevic <srdjan.miljevic@sbgenomics.com>
2017-12-26 12:13:59 +0100

    handle unknown job record in input/output event handlers

--- rabix-engine/src/main/java/org/rabix/engine/processor/handler/impl/ContextStatusEventHandler.java
+++ rabix-engine/src/main/java/org/rabix/engine/processor/handler/impl/ContextStatusEventHandler.java
@@ -1,14 +1,17 @@
 package org.rabix.engine.processor.handler.impl;
 
+import com.google.inject.Inject;
 import org.rabix.engine.event.impl.ContextStatusEvent;
-import org.rabix.engine.store.model.ContextRecord;
 import org.rabix.engine.processor.handler.EventHandler;
 import org.rabix.engine.processor.handler.EventHandlerException;
 import org.rabix.engine.service.ContextRecordService;
-
-import com.google.inject.Inject;
+import org.rabix.engine.store.model.ContextRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ContextStatusEventHandler implements EventHandler<ContextStatusEvent> {
+
+  private static final Logger logger = LoggerFactory.getLogger(ContextStatusEventHandler.class);
 
   private final ContextRecordService contextRecordService;
 
@@ -20,6 +23,11 @@ public class ContextStatusEventHandler implements EventHandler<ContextStatusEven
   @Override
   public void handle(ContextStatusEvent event, EventHandlingMode mode) throws EventHandlerException {
     ContextRecord contextRecord = contextRecordService.find(event.getContextId());
+    if (contextRecord == null) {
+      logger.warn("Unknown context {}. Skipping...", event.getContextId());
+      return;
+    }
+
     contextRecord.setStatus(event.getStatus());
     contextRecordService.update(contextRecord);
   }
