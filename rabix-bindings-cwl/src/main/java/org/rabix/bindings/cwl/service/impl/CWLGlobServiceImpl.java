ec1b3978730ee5e58e83d6f13527a1af5c09032a
Milos Ljubinkovic <milos.ljubinkovic@sbgenomics.com>
2017-12-26 17:11:15 +0100

    Glob sorting

--- rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/service/impl/CWLGlobServiceImpl.java
+++ rabix-bindings-cwl/src/main/java/org/rabix/bindings/cwl/service/impl/CWLGlobServiceImpl.java
@@ -2,13 +2,17 @@ package org.rabix.bindings.cwl.service.impl;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.rabix.bindings.cwl.bean.CWLJob;
@@ -48,7 +52,7 @@ public class CWLGlobServiceImpl implements CWLGlobService {
       globs.add((String) glob);
     }
 
-    final Set<Path> files = new LinkedHashSet<>();
+    final Set<Path> files = new TreeSet<>();
     for (String singleGlob : globs) {
       if (singleGlob.equals(".")) {
         files.add(workingDir);
